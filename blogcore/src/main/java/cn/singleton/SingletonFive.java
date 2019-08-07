ca3304d2ed732bbc43896ed9fc57670c07675243
kzhou <Kaidong.Zhou@eisgroup.com>
2019-08-07 11:02:27 +0800

    fix branch2

---
+++ blogcore/src/main/java/cn/singleton/SingletonFive.java
@@ -0,0 +1,21 @@
+﻿package cn.singleton;
+
+/**
+ *  单例模式下处理多线程方法3  双重检查加锁
+ */
+public class SingletonFive{
+    private volatile static SingletonFive singletonOne;
+    private SingletonFive(){
+
+    }
+    public  static SingletonFivegetInstance(){
+        if(singletonOne ==null){
+            synchronized (SingletonFive.class){
+                if(singletonOne==null){
+                    singletonOne = new SingletonFive();
+                }
+            }
+        }
+        return singletonOne;
+    }
+}
